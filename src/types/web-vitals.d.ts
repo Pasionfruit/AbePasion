a0e9c8bd428a3df56200a0657324063c0b5dbf6f
Abe Pasion <pasionabe@gmail.com>
2025-04-30 21:54:41 -0400

    Moves portfolio

--- src/types/web-vitals.d.ts
+++
@@ -1,8 +0,0 @@
-declare module 'web-vitals' {
-  export type ReportHandler = (metric: any) => void;
-  export function getCLS(onPerfEntry?: ReportHandler): void;
-  export function getFID(onPerfEntry?: ReportHandler): void;
-  export function getFCP(onPerfEntry?: ReportHandler): void;
-  export function getLCP(onPerfEntry?: ReportHandler): void;
-  export function getTTFB(onPerfEntry?: ReportHandler): void;
-} 
